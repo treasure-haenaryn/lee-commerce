48164db2b638ffd5cb358bfbe4e15c0a439d1b0e
treasure-haenaryn <wotjr7386@naver.com>
2026-08-15 19:44:38 +0900

    chore(user): Web/Validation 의존성 추가
    user 모듈 안에 Interface 레이어(Controller)를 두기로 해서 spring-boot-starter-web,
    spring-boot-starter-validation을 추가한다.

--- user/build.gradle.kts
+++ user/build.gradle.kts
@@ -1,6 +1,8 @@
 dependencies {
     implementation(project(":common"))
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.security:spring-security-crypto")
     implementation(libs.redisson.spring.boot.starter)
 
