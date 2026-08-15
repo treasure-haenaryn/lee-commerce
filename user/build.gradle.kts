3b372cf3976e0f5ff92696249023954298e1fc6a
treasure-haenaryn <wotjr7386@naver.com>
2026-08-15 17:55:39 +0900

    chore(user): Lombok 의존성 추가
    Application 레이어부터 rules/*.md 예제 코드 컨벤션대로 @RequiredArgsConstructor를
    쓴다. 이미 커밋된 Domain 클래스(수동 getter)는 스타일만 바꾸는 리팩터링이라 손대지
    않았다.

--- user/build.gradle.kts
+++ user/build.gradle.kts
@@ -1,4 +1,7 @@
 dependencies {
     implementation(project(":common"))
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
 }
